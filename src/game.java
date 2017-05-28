6375f5b70616d6ccb56b926b3ece76eda17e268e
onf <qwerty2945@outlook.com>
2017-05-27 22:08:46 -0400

    program specific
    added .class files, moved code to src folder

--- src/game.java
+++ src/game.java
@@ -1,8 +1,8 @@
 
 /**
  * Write a description of class game here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  *
  *
@@ -22,24 +22,24 @@ public class game
         Board[][] TTT = new Board[3][3];
         int count = 1;
         int turn = 2;
-        
+
         System.out.print("Please set the window to fullscreen. Press ENTER to continue... "); read.nextLine();
         drawTutorial1();
         System.out.print("\nPress ENTER to continue... "); read.nextLine();
         drawTutorial2();
         System.out.print("\nPress ENTER to start the game... "); read.nextLine();
         System.out.print("\f");
-        
+
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 TTT[i][j] = new Board(count);
                 count++;
             }
         }
-        
+
         System.out.println("Player " + (turn%2+1) + "'s turn.");
         drawBoard(TTT);
-        
+
         int subBoard, tile;
         System.out.print("Choose a sub-board: "); subBoard = read.nextInt();
         while (subBoard < 1 || subBoard > 9) {
@@ -57,7 +57,7 @@ public class game
         TTT[r][c].move(tile, (turn%2==0)?"X":"O");
         System.out.print("\f");
         drawBoard(TTT);
-        
+
         while(true) {
             turn++;
             subBoard = tile;
@@ -73,12 +73,12 @@ public class game
                 r = Integer.parseInt(temp.substring(0,1));
             }
             System.out.print("\f");
-            
+
             System.out.println("Player " + (turn%2+1) + "'s turn.");
             drawBoard(TTT);
         }
     }
-    
+
     //finds index of the board number chosen
     public static String findIndexOfSubBoard(int subBoard) {
         for(int i = 0; i < 3; i++) {
@@ -91,7 +91,7 @@ public class game
         }
         return null;
     }
-    
+
     public static void drawBoard(Board[][] TTT) {
         System.out.println("#######################################");
         for(int i = 0; i < 3; i++) {
@@ -104,51 +104,51 @@ public class game
             System.out.println("#######################################");
         }
     }
-    
+
     public static void drawTutorial1() {
         System.out.println("#######################################\n"+
-                           "#           ##           ##           #\n"+
-                           "#           ##           ##           #\n"+
-                           "#     1     ##     2     ##     3     #\n"+
-                           "#           ##           ##           #\n"+
-                           "#           ##           ##           #\n"+
-                           "#######################################\n"+
-                           "#           ##           ##           #\n"+
-                           "#           ##           ##           #\n"+
-                           "#     4     ##     5     ##     6     #\n"+
-                           "#           ##           ##           #\n"+
-                           "#           ##           ##           #\n"+
-                           "#######################################\n"+
-                           "#           ##           ##           #\n"+
-                           "#           ##           ##           #\n"+
-                           "#     7     ##     8     ##     9     #\n"+
-                           "#           ##           ##           #\n"+
-                           "#           ##           ##           #\n"+
-                           "#######################################\n");
+                "#           ##           ##           #\n"+
+                "#           ##           ##           #\n"+
+                "#     1     ##     2     ##     3     #\n"+
+                "#           ##           ##           #\n"+
+                "#           ##           ##           #\n"+
+                "#######################################\n"+
+                "#           ##           ##           #\n"+
+                "#           ##           ##           #\n"+
+                "#     4     ##     5     ##     6     #\n"+
+                "#           ##           ##           #\n"+
+                "#           ##           ##           #\n"+
+                "#######################################\n"+
+                "#           ##           ##           #\n"+
+                "#           ##           ##           #\n"+
+                "#     7     ##     8     ##     9     #\n"+
+                "#           ##           ##           #\n"+
+                "#           ##           ##           #\n"+
+                "#######################################\n");
         System.out.println("Each section of the board is assigned a number. Use these numbers to choose a sub-board at the start of the game.");
-        
+
     }
     public static void drawTutorial2() {
         System.out.println("#######################################\n"+
-                           "# 1 | 2 | 3 ## 1 | 2 | 3 ## 1 | 2 | 3 #\n"+
-                           "#-----------##-----------##-----------#\n"+
-                           "# 4 | 5 | 6 ## 4 | 5 | 6 ## 4 | 5 | 6 #\n"+
-                           "#-----------##-----------##-----------#\n"+
-                           "# 7 | 8 | 9 ## 7 | 8 | 9 ## 7 | 8 | 9 #\n"+
-                           "#######################################\n"+
-                           "# 1 | 2 | 3 ## 1 | 2 | 3 ## 1 | 2 | 3 #\n"+
-                           "#-----------##-----------##-----------#\n"+
-                           "# 4 | 5 | 6 ## 4 | 5 | 6 ## 4 | 5 | 6 #\n"+
-                           "#-----------##-----------##-----------#\n"+
-                           "# 7 | 8 | 9 ## 7 | 8 | 9 ## 7 | 8 | 9 #\n"+
-                           "#######################################\n"+
-                           "# 1 | 2 | 3 ## 1 | 2 | 3 ## 1 | 2 | 3 #\n"+
-                           "#-----------##-----------##-----------#\n"+
-                           "# 4 | 5 | 6 ## 4 | 5 | 6 ## 4 | 5 | 6 #\n"+
-                           "#-----------##-----------##-----------#\n"+
-                           "# 7 | 8 | 9 ## 7 | 8 | 9 ## 7 | 8 | 9 #\n"+
-                           "#######################################\n");
+                "# 1 | 2 | 3 ## 1 | 2 | 3 ## 1 | 2 | 3 #\n"+
+                "#-----------##-----------##-----------#\n"+
+                "# 4 | 5 | 6 ## 4 | 5 | 6 ## 4 | 5 | 6 #\n"+
+                "#-----------##-----------##-----------#\n"+
+                "# 7 | 8 | 9 ## 7 | 8 | 9 ## 7 | 8 | 9 #\n"+
+                "#######################################\n"+
+                "# 1 | 2 | 3 ## 1 | 2 | 3 ## 1 | 2 | 3 #\n"+
+                "#-----------##-----------##-----------#\n"+
+                "# 4 | 5 | 6 ## 4 | 5 | 6 ## 4 | 5 | 6 #\n"+
+                "#-----------##-----------##-----------#\n"+
+                "# 7 | 8 | 9 ## 7 | 8 | 9 ## 7 | 8 | 9 #\n"+
+                "#######################################\n"+
+                "# 1 | 2 | 3 ## 1 | 2 | 3 ## 1 | 2 | 3 #\n"+
+                "#-----------##-----------##-----------#\n"+
+                "# 4 | 5 | 6 ## 4 | 5 | 6 ## 4 | 5 | 6 #\n"+
+                "#-----------##-----------##-----------#\n"+
+                "# 7 | 8 | 9 ## 7 | 8 | 9 ## 7 | 8 | 9 #\n"+
+                "#######################################\n");
         System.out.println("Each sub-board is split into tiles from 1-9. Players take turns using these numbers to choose a spot to mark during the game." +
-                           "\nWin 3 sub-boards horizontally, vertically, or diagonally to win the game.");
+                "\nWin 3 sub-boards horizontally, vertically, or diagonally to win the game.");
     }
 }
